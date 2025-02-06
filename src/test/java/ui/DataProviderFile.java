5fc7e050aa115ba62873b8cac7c0fb81d39f28b6
Anuj Agarwal <agarwalanuj132001@gmail.com>
2025-02-06 17:53:06 +0530

    updated

--- src/test/java/ui/DataProviderFile.java
+++ src/test/java/ui/DataProviderFile.java
@@ -9,6 +9,8 @@ public class DataProviderFile {
 	Object[][] dataset = {{"standard_user","secret_sauce"},{"locked_out_user","secret_sauce"},{"problem_user","secret_sauce"}, {"performance_glitch_user","secret_sauce"}};
 	
 	return dataset;
+	
+	
 	}
 
 }
